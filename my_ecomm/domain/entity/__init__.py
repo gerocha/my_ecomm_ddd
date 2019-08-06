9413c36ff3c1fd45343ce79f9d6c57443baf48ae
Gelso <gelso.rocha@wllm.com.br>
2019-08-06 06:35:20 -0300

    inserting default imports under __init__ files

--- my_ecomm/domain/entity/__init__.py
+++ my_ecomm/domain/entity/__init__.py
@@ -1,3 +1,4 @@
 from .customer import *
 from .product import *
 from .shopping_cart import *
+from .order import *
---
+++ my_ecomm/domain/entity/interfaces/__init__.py
@@ -0,0 +1,5 @@
+from .address import IAddress
+from .customer import ICustomer
+from .order import IOrder
+from .product import IProduct
+from .shopping_cart import IShoppingCart
