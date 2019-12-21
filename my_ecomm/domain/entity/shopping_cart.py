c3000e04581f50d6c985d1ebb10f8b6f7d45a0b6
Gelso <gelso.rocha@wllm.com.br>
2019-12-20 23:31:00 -0300

    adding generate_order to shopping cart

--- my_ecomm/domain/entity/shopping_cart.py
+++ my_ecomm/domain/entity/shopping_cart.py
@@ -44,4 +44,7 @@ class ShoppingCart(IShoppingCart):
         self.item_list = []
 
     def generate_order(self):
-        return Order(uuid='teste')
+        order = Order(uuid='teste', items=[item.item for item in self.item_list])
+        self.clear()
+
+        return order
